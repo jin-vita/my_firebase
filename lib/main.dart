ac17cc4d13cf965517b6f5e617cfa2d08f8e1b31
VITA-R9 <jhj9144@gmail.com>
2023-03-01 00:34:27 +0900

    basic setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'firebase_options.dart';
 
-void main() {
+// flutter pub add get
+// flutter pub add firebase_core
+// flutter pub add firebase_auth
+// flutter pub add firebase_database
+// flutter pub add firebase_messaging
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -10,7 +22,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
